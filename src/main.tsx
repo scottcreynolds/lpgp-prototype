6bf2fd62384454fc9c6f6b129a4058ac89a8068e
Scott C. Reynolds <scott@scottcreynolds.com>
2025-11-04 13:39:12 -0500

    initialize and add chakra-ui

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { Provider } from "@/components/ui/provider";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </StrictMode>
+);
